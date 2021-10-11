b0f48f2852f394904282ab202d5961a68e232796
Aruksh Patel <arukshpatel98@gmail.com>
2021-10-11 14:32:01 -0500

    added GUILDS and partials to receive messages

--- bot.js/index.js
+++ bot.js/index.js
@@ -1,17 +1,30 @@
 "use strict";
-require('dotenv').config();
+require("dotenv").config();
 var _TOKEN = String(process.env.TOKEN);
 var Discord = require("discord.js");
 var client = new Discord.Client({
-    intents: [Discord.Intents.FLAGS.DIRECT_MESSAGES, Discord.Intents.FLAGS.GUILD_MESSAGES]
+    /*
+    Intents 'GUILDS' is required
+    if you wish to receive (message) events
+    from guilds as well.
+    */
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.DIRECT_MESSAGES,
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
+    ],
+    /*
+    You have to include partials
+    to recieve messages from Channel and DM
+    */
+    partials: ["MESSAGE", "CHANNEL"],
 });
 client.on("ready", function (msg) {
-    console.log('ChuggBot logged in as ' + msg.user.tag);
-    // msg.reply("It works")
+    console.log("ChuggBot logged in as " + msg.user.tag);
 });
-client.on("message", function (msg) {
-    if (msg.startsWith('$')) {
-        console.log(msg);
-    }
+client.on("messageCreate", function (msg) {
+    console.log(msg);
+    //FIXME
+    msg.reply("At the moment, I cannot do anything. Please mix me");
 });
 client.login(_TOKEN);
--- bot.ts/index.ts
+++ bot.ts/index.ts
@@ -1,21 +1,36 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const _TOKEN: string = String(process.env.TOKEN);
 const Discord = require("discord.js");
 
 const client = new Discord.Client({
-    intents: [Discord.Intents.FLAGS.DIRECT_MESSAGES, Discord.Intents.FLAGS.GUILD_MESSAGES]
-})
+    /*
+    Intents 'GUILDS' is required
+    if you wish to receive (message) events
+    from guilds as well.
+    */
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.DIRECT_MESSAGES,
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
+    ],
+
+    /*
+    You have to include partials
+    to recieve messages from Channel and DM
+    */
+    partials: ["MESSAGE", "CHANNEL"],
+});
 
 client.on("ready", (msg: any) => {
-    console.log('ChuggBot logged in as ' + msg.user.tag)
-    // msg.reply("It works")
-})
+    console.log("ChuggBot logged in as " + msg.user.tag);
+});
 
-client.on("message", (msg: string) => {
-    if (msg.startsWith('$')) {
-        console.log(msg);
-    }
-})
+client.on("messageCreate", (msg: any) => {
+    console.log(msg);
+
+    //FIXME
+    msg.reply("At the moment, I cannot do anything. Please mix me");
+});
 
 client.login(_TOKEN);
