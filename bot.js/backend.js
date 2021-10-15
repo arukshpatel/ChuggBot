fd1e938946d4be68df4a350c488d601093a41b59
Aruksh Patel <arukshpatel98@gmail.com>
2021-10-14 23:37:03 -0500

    added arguments for chromedriver

--- bot.js/backend.js
+++ bot.js/backend.js
@@ -1,11 +1,41 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.fetchURL = void 0;
+require("chromedriver");
+const { By, Key, Builder, Capabilities } = require("selenium-webdriver");
 async function fetchURL(url) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve("resolved");
-        }, 7000);
+    return new Promise(async (resolve, reject) => {
+        if (url == undefined) {
+            reject("URL is unreadable/undefined");
+        }
+        try {
+            var chromeCapabiities = Capabilities.chrome();
+            var chromeOptions = {
+                args: [""],
+            };
+            chromeCapabiities.set("chromeOptions", chromeOptions);
+            var driver = await new Builder()
+                .forBrowser("chrome")
+                .withCapabilities(chromeCapabiities)
+                .build();
+            console.log(driver.get);
+        }
+        catch (error) {
+            reject("Chrome driver could not be initiated");
+        }
+        let searchString = "selenium";
+        //To wait for browser to build and launch properly
+        // let driver = await new Builder().forBrowser("chrome").build();
+        //To fetch http://google.com from the browser with our code.
+        await driver.get(url);
+        //To send a search query by passing the value in searchString.
+        // await driver.findElement(By.name("q")).sendKeys(searchString, Key.RETURN);
+        //Verify the page title and print it
+        var title = await driver.getTitle();
+        // console.log("Title is:", title);
+        //It is always a safe practice to quit the browser after execution
+        await driver.quit();
+        resolve(title);
     });
 }
 exports.fetchURL = fetchURL;
--- bot.ts/backend.ts
+++ bot.ts/backend.ts
@@ -1,9 +1,45 @@
-async function fetchURL(url: string) {
-    return new Promise<String>((resolve, reject) => {
-        setTimeout(() => {
-            resolve("resolved");
-        }, 7000);
+require("chromedriver");
+const { By, Key, Builder, Capabilities } = require("selenium-webdriver");
+
+export async function fetchURL(url?: string) {
+    return new Promise<String>(async (resolve, reject) => {
+        if (url == undefined) {
+            reject("URL is unreadable/undefined");
+        }
+        try {
+            var chromeCapabiities = Capabilities.chrome();
+            var chromeOptions = {
+                args: [""],
+            };
+            chromeCapabiities.set("chromeOptions", chromeOptions);
+            var driver = await new Builder()
+                .forBrowser("chrome")
+                .withCapabilities(chromeCapabiities)
+                .build();
+
+            console.log(driver.get);
+        } catch (error) {
+            reject("Chrome driver could not be initiated");
+        }
+
+        let searchString = "selenium";
+
+        //To wait for browser to build and launch properly
+        // let driver = await new Builder().forBrowser("chrome").build();
+
+        //To fetch http://google.com from the browser with our code.
+        await driver.get(url);
+
+        //To send a search query by passing the value in searchString.
+        // await driver.findElement(By.name("q")).sendKeys(searchString, Key.RETURN);
+
+        //Verify the page title and print it
+        var title = await driver.getTitle();
+        // console.log("Title is:", title);
+
+        //It is always a safe practice to quit the browser after execution
+        await driver.quit();
+
+        resolve(title);
     });
 }
-
-export { fetchURL };
