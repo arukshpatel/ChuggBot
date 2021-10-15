8d5195e2b8e1cc09f08323be24766bd4378357b3
Aruksh Patel <arukshpatel98@gmail.com>
2021-10-14 23:28:02 -0500

    exported custom reponses

---
+++ bot.js/customResponses.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports._Todo = exports.helpCodes = exports.defaultResponse = void 0;
+exports.defaultResponse = "Please enter valid argument. Type `~help / ~help-code(s)` for more info";
+exports.helpCodes = "Commands should be the following format:\n" +
+    "`~[help-code] [corresponding-text]`\n" +
+    "List of Help Codes: \n" +
+    "```~chugg [URL]\n" +
+    "~medium [URL]\n" +
+    "~help / ~help-code(s) [*leave blank*]```";
+exports._Todo = "This function has yet to be implemented, sorry.";
---
+++ bot.ts/customResponses.ts
@@ -0,0 +1,12 @@
+export const defaultResponse: string =
+    "Please enter valid argument. Type `~help / ~help-code(s)` for more info";
+
+export const helpCodes: string =
+    "Commands should be the following format:\n" +
+    "`~[help-code] [corresponding-text]`\n" +
+    "List of Help Codes: \n" +
+    "```~chugg [URL]\n" +
+    "~medium [URL]\n" +
+    "~help / ~help-code(s) [*leave blank*]```";
+
+export const _Todo: string = "This function has yet to be implemented, sorry.";
